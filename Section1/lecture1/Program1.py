480f59064fe631887bee94bfbb46c6fc834b8164
david paul <david00000013@gmail.com>
2022-10-06 07:06:18 +0530

    section1 commit

---
+++ Section1/lecture1/Program1.py
@@ -0,0 +1,11 @@
+# Python program to check a number is even or odd
+def checkEven(num):
+    if num%2 == 0:
+        return True
+    else:
+        return False
+n = int(input("Enter Number \n"))
+if(checkEven(n)):
+    print(n," is Even")
+else:
+    print(n," is Odd")
